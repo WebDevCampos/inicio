5c847644eded1d61a06d6ed6537fa6b14f09d3a3
MarcusViniciusTavaresRangel <marcus_trangel@yahoo.com.br>
2021-01-03 18:28:30 -0300

    added index.js

--- index.js
+++ index.js
@@ -7,22 +7,3 @@ class MeuH1 extends HTMLElement{
 
 customElements.define('meu-h1', MeuH1);
 
-const shadowRoot = document.querySelector('#inicio').attachShadow({mode:'open'});
-shadowRoot.innerHTML = `
-<style>
-
-    *{
-        margin:0;
-        padding:0;
-        box-sizing: border-box;
-        clear:both;
-
-    }
-.inicio{
-    width:100%;
-    background-color:lightgrey;
-}
-
-</style>
-
-<div class="inicio"><p style="color:blue;">odoj</p>`
