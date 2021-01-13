152ec2d71220c6e78a5dadb413b2a0c03caeef65
MarcusViniciusTavaresRangel <marcus_trangel@yahoo.com.br>
2021-01-12 21:43:06 -0300

    added

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <body>
 
 Welcome <?php echo $_POST['name']; ?><br>
-Your email address is: <?php echo $_POST['email']; ?>
-
+Your email address is: <?php echo $_POST['email']; ?><br>
+<?php echo "Ah muleque" ?>
 </body>
 </html>
