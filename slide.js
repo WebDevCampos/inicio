f39de12013c4991a1d005722feff56c40e9bb425
MarcusViniciusTavaresRangel <marcus_trangel@yahoo.com.br>
2021-01-20 06:31:08 -0300

    added

--- slide.js
+++ slide.js
@@ -1,39 +1,5 @@
 
-     $(document).ready(function(){
-         $('.slider').slick({
-             outline:false,
-             arrows:false,
-             autoplay: false,
-             slidesToShow: 3,
-             adaptiveHeight: true,
-             responsive: [{
-                 breakpoint: 768,
-                 settings: {
-                 slidesToShow: 2,
-                 infinite: false,
-                 
-             }
-                 }, {
-                 breakpoint: 451,
-                 settings: {
-                 slidesToShow: 1,
-                 infinite: false,
-                 }
-             }]
-         });
-     });
+   
 
 
 
-var cards = $('.card-body');
-var maxHeight = 0;
-
-for (var i = 0; i < cards.length; i++) {
-if (maxHeight < $(cards[i]).outerHeight()) {
-maxHeight = $(cards[i]).outerHeight();
-}
-}
-
-for (var i = 0; i < cards.length; i++) {
-$(cards[i]).height(maxHeight);
-}
