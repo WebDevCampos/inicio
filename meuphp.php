bb982493a56989c84d89f66640271d773f05bc2b
Marcus Rangel <webdevcampos@gmail.com>
2021-03-09 14:03:38 -0300

    Modified

---
+++ meuphp.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+    <title></title>
+    <meta name="" content="">
+    <style></style>
+</head>
+
+
+<body>
+Nome
+<input name="nome" >
+<button type='submit'>Enviar</button>
+
+
+</form>
+<?php 
+
+$nome = $_POST['nome'];
+
+echo "Olá, [$nome]."
+
+?>
+</body>
+</html>
