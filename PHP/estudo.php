f711249de93a456a90fe20d21077a727bf5e5a00
Marcus Vinicius Tavares Rangel <webdevcampos@gmail.com>
2021-04-12 19:56:27 -0300

    Modified

--- PHP/estudo.php
+++ PHP/estudo.php
@@ -1,78 +1,88 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="UTF-8">
-    <title>Portfolio</title>
+    <title>Padrão</title>
     
-   <!--BS-->
+<!--#region BS-->
    
     
-   <link rel="stylesheet" href="./bootstrap.min.css">
+   <link rel="stylesheet" href="bootstrap.min.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
  
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-   <!--BS-->
+<!--#endregion BS-->
    
-   <!--SLICK-->
+
+<!--#region SLICK-->
  
- <link rel="stylesheet" href="./slick.css"/>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+<link rel="stylesheet" href="slick.css"/>
     
-   <!--SLICK-->
+<!--#endregion SLICK-->
 
  
-<!--FA-->
- <link rel="stylesheet" href="all.css" />
- <link rel="stylesheet" href="all.min.css" />
+<!--#region FA-->
  
- <script src="https://kit.fontawesome.com/dbce8a62cb.js" crossorigin="anonymous"></script>
-<!--FA-->
+<script src="https://kit.fontawesome.com/dbce8a62cb.js" crossorigin="anonymous"></script>
 
- 
- <!--Fonts-->
- <link rel="stylesheet" type="text/css"
- href="./Fontes/gitaluevo-cufonfonts-webfont/style.css"/>
+<!--#endregion FA-->
 
- <link rel="stylesheet" type="text/css"
- href="./Fontes/HomeSchool/style.css">
-
- 
- <link rel="stylesheet" type="text/css"
- href="./Fontes/miso-cufonfonts-webfont/style.css">
-
- 
- <link rel="stylesheet" type="text/css"
- href="./Fontes/vtks-realm-cufonfonts-webfont/style.css">
-
- 
- <link rel="stylesheet" type="text/css"
- href="./Fontes/wanoquin-free-pu-cufonfonts-webfont/style.css">
- <!--Fonts-->
-    <style>
+ </head>
+ <body >
 
 
-
-    </style>
-</head>
-
-    <body >
-     
- <?php echo "Yes" ?>
-
-    
-    <script src="./jquery-3.5.1.js"></script>
-    <script src="./jquery-3.5.1.min.js"></script>
-    <script src="./bootstrap.min.js" crossorigin="anonymous"></script>
-    
-    <script src="./slick.min.js"></script>
-    <script src="./slick.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
-      
-<script>
 
   
-</script>
+<!--#region Scripts -->
 
-      </body>
+<!--#region JQuery-->
+  <script src="jquery-3.6.0.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"></script>
+
+<!--#endregion JQuery-->  
+
+<!--#region BS-->
+  <script src="bootstrap.min.js" crossorigin="anonymous"></script>
+  
+<!--#endregion BS-->  
+
+<!--#region Slick-->
+
+  <script src="slick.min.js"></script>
+  <script src="slick.js"></script>
+  <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js">
+  </script>
+
+<!--#endregion Slick-->
+
+<!--#region Vue-->
+
+  <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+
+<!--#endregion Vue-->  
+
+
+<!--#region Mine JS-->
+  <script>
+  $(document).ready(function(){
+    $('.carro').slick({
+      infinite:true,
+      arrows:true,
+      dots:true,
+      autoplay:true,
+    })
+  })
+  </script>
+
+<!--#endregion Mine JS-->
+
+<!--#endregion Scripts-->
+  </body>
 </html>
