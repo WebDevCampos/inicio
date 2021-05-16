02d7bd5bc4d21b22814ea40351863502469a9e94
Marcus Vinicius Tavares Rangel <webdevcampos@gmail.com>
2021-05-16 11:55:51 -0300

    Modified

---
+++ treino/padrao.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+Vue.component('header-nome',{
+    template: `
+    <div class='container-fluid bg-danger text-white text-center p-4 fs-1'>
+    <p>{{name}}</p>
+    </div>`,
+    
+    data(){
+      return {
+        name: 'Marcus'
+      }
+    }
+   
+  })
+  
+  const STDheader = new Vue({
+    el:'#STDheader',
+  })
+  
+
+
+  $(document).ready(function(){
+    $('.carro').slick({
+      infinite:true,
+      arrows:true,
+      dots:true,
+      autoplay:true,
+    })
+  })
+
+
+  
