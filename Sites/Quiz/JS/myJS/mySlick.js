7d4ba31e59dcad210025a9717436ac9aa44cf948
Marcus Vinicius Tavares Rangel <webdevcampos@gmail.com>
2021-07-03 11:26:17 -0300

    Added correct folders

---
+++ Sites/Quiz/JS/myJS/mySlick.js
@@ -0,0 +1,11 @@
+//#region slick
+$("document").ready(function () {
+  $(".carro_um").slick({
+    arrows: true,
+    appendArrows: $(".meuPrevArrow"),
+    appendArrows: $(".meuNextArrow"),
+    prevArrow: $(".meuPrevArrow"),
+    nextArrow: $(".meuNextArrow"),
+  });
+});
+//#endregion slick
